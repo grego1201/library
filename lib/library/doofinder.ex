e072157cd26bfb313ca773b1830cac9822e2bb86
Gregorio Patiño <gregoriopatinoesteo@gmail.com>
2024-01-19 12:06:13 +0100

    Add doofinder context. Generate books feed

---
+++ lib/library/doofinder.ex
@@ -0,0 +1,33 @@
+defmodule Library.Doofinder do
+  @moduledoc """
+  The Editorial context.
+  """
+
+  alias Library.Books
+
+  @books_feed_headers [brand: :brand, group_id: :group_id, id: :id, cover_image_link: :cover_image_link, link: :link, title: :title]
+  @books_feed_path "priv/static/feed_data/feed_data.csv"
+
+  def generate_books_feed() do
+    file = File.open!(@books_feed_path, [:write])
+    write_books_feed_body(file)
+  end
+
+  defp write_books_feed_body(file) do
+    books = Books.list_books
+    mapped_books = Enum.map(books, fn book -> book_to_row(book) end)
+    csv_content = mapped_books |> CSV.encode(headers: @books_feed_headers) |> Enum.to_list()
+    IO.puts(file, csv_content)
+  end
+
+  defp book_to_row(book) do
+    parsed_book = %{
+      brand: book.publisher.name,
+      group_id: book.publisher.id,
+      id: book.id,
+      cover_image_link: book.cover_image_path,
+      link: "https://foo.com",
+      title: book.title,
+    }
+  end
+end
